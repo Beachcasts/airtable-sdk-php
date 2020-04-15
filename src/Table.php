4460d4f94bc3793bf114f6fe3e280ca6a0f8b56a
Adam Culp <adamculp@uws.net>
2020-04-15 18:48:16 -0400

    Added CRUD+L methods
    Signed-off-by: Adam Culp <adamculp@uws.net>

--- src/Table.php
+++ src/Table.php
@@ -20,34 +20,126 @@ class Table
     /**
      * @var string|null
      */
-    public $name = null;
+    protected $name = null;
+
+    protected $client;
+
+    protected $viewName;
 
     /**
      * Table constructor.
+     *
      * @param string $name
+     * @param string $viewName
      */
-    public function __construct(string $name)
+    public function __construct(string $name, string $viewName = "Grid view")
     {
         $this->name = $name;
+        $this->viewName = $viewName;
     }
 
     /**
-     * @param Client $connection
-     * @param string $view
+     * @param Client $client
+     */
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
      * @return ResponseInterface
      */
-    public function list(Client $connection, string $view = "Grid view"): ResponseInterface
+    public function list(): ResponseInterface
     {
-        $list = $connection->request(
+        return $this->client->request(
             'GET',
-            $this->name . '?maxRecords=3&view=' . $view,
+            $this->name . '?maxRecords=3&view=' . $this->viewName,
             [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $_ENV['API_KEY'],
                 ]
             ]
         );
+    }
 
-        return $list;
+    /**
+     * @param string $data
+     * @return mixed
+     */
+    public function create(string $data)
+    {
+        return $this->client->request(
+            'POST',
+            $this->name,
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $_ENV['API_KEY'],
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => $data,
+            ]
+        );
+    }
+
+    /**
+     * @param string $id
+     * @return mixed
+     */
+    public function read(string $id)
+    {
+        return $this->client->request(
+            'GET',
+            $this->name . DIRECTORY_SEPARATOR . $id,
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $_ENV['API_KEY'],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function update(string $data)
+    {
+        return $this->client->request(
+            'PATCH',
+            $this->name,
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $_ENV['API_KEY'],
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => $data,
+            ]
+        );
+    }
+
+    /**
+     * @param string $id
+     * @return mixed
+     */
+    public function delete(string $id)
+    {
+        return $this->client->request(
+            'DELETE',
+            $this->name,
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $_ENV['API_KEY'],
+                ],
+                'query' => ['records[]' => $id],
+            ]
+        );
     }
 }
